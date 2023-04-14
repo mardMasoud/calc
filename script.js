8d888d3d4e8a4c15c80853f4c9dae1f848f8cfd5
masoudMarde <mardmasoud66@gmail.com>
2023-04-15 02:33:17 +0430

    randomBack

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-var todolist = [
-  { id: 1, name: "reading book", status: true },
-  { id: 2, name: "lunch", status: false },
-  { id: 3, name: "writing", status: true },
-  { id: 4, name: "sporting", status: true },
-  { id: 5, name: "worker", status: false },
-  { id: 6, name: "eating", status: false },
-];
+const itemElm = document.querySelectorAll(".items");
+//let colors = ['red','green','blue','black','orange']
+let rcolorIndex, gcolorIndex, bcolorIndex;
 
-var menu;
-var x1 = {};
-menu = prompt("1: add\n2: remove\n3: doing");
-console.log(menu);
+//itemElm[2].style.backgroundColor='blue'
 
-if (menu === "1") {
-  var name1 = prompt("نام تودو را وارد کنید");
+  setInterval(function () {
+        itemElm.forEach(function(item){
+    rcolorIndex = Math.floor(Math.random() * 255);
+    gcolorIndex = Math.floor(Math.random() * 255);
+    bcolorIndex = Math.floor(Math.random() * 255);
+    
+    item.style.backgroundColor =
+      "rgb(" + rcolorIndex + "," + gcolorIndex + "," + bcolorIndex + ")";
+      item.textContent= item.style.backgroundColor
+          }  )
+  }, 4000);
 
-  newTodoObject = { id: todolist.length + 1, name: name1, status: false };
-  todolist.push(newTodoObject);
-} else if (menu === "2") {
-  var name1 = prompt("نام تودویی که میخواهید حذف شود را وارد کنید");
-  var removeTodo = todolist.findIndex((item) => item.name === name1);
-  todolist.splice(removeTodo, 1);
-} else if (menu === "3") {
-  var name1 = prompt("نام تودو را وارد کنید");
-  todolist.forEach(function (item) {
-    if (item.name === name1) item.status = true;
-  });
-} else {
-  alert("گزینه انتخابی درست نیست");
-}
-console.log(todolist);
