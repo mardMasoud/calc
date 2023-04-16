d5127ba9c30deaa29257da726b491cb6a5a80130
mardMasoud <mardmasoud66@google.com>
2023-04-16 16:31:32 +0330

    fly

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-var todolist = [
-  { id: 1, name: "reading book", status: true },
-  { id: 2, name: "lunch", status: false },
-  { id: 3, name: "writing", status: true },
-  { id: 4, name: "sporting", status: true },
-  { id: 5, name: "worker", status: false },
-  { id: 6, name: "eating", status: false },
-];
+let $ = document;
+let countreis = {
+  iran: ["tehran", "mashhad", "sari", "esfahan"],
+  usa: ["washington", "los angels", "San Francisco", "New York"],
+  england: ["Oxford", "London", "Manchester", "Birmingham"],
+  france: ["Paris", "Lyon", "Toulouse", "Bordeaux"],
+};
 
-var menu;
-var x1 = {};
-menu = prompt("1: add\n2: remove\n3: doing");
-console.log(menu);
+let dataCountreis = $.querySelector("#countries");
+let dataCiteis = $.querySelector("#citeis");
 
-if (menu === "1") {
-  var name1 = prompt("نام تودو را وارد کنید");
-
-  newTodoObject = { id: todolist.length + 1, name: name1, status: false };
-  todolist.push(newTodoObject);
-} else if (menu === "2") {
-  var name1 = prompt("نام تودویی که میخواهید حذف شود را وارد کنید");
-  var removeTodo = todolist.findIndex((item) => item.name === name1);
-  todolist.splice(removeTodo, 1);
-} else if (menu === "3") {
-  var name1 = prompt("نام تودو را وارد کنید");
-  todolist.forEach(function (item) {
-    if (item.name === name1) item.status = true;
-  });
-} else {
-  alert("گزینه انتخابی درست نیست");
-}
-console.log(todolist);
+let country;
+dataCountreis.addEventListener("change", function (e) {
+  country = e.target.value;
+  dataCiteis.value='esfahan'
+});
